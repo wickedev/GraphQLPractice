3c950c7b8524b214e4b006b418b925c259039d34
wickedev <orange881217@gmail.com>
2021-04-01 11:31:01 +0900

    add getValueFromDataLoader extension form kclass

--- src/main/kotlin/org/example/entity/Post.kt
+++ src/main/kotlin/org/example/entity/Post.kt
@@ -4,7 +4,7 @@ import graphql.schema.DataFetchingEnvironment
 import org.example.dataloader.AuthorDataLoader
 import org.example.util.DEFAULT_ID_VALUE
 import org.example.util.Identifier
-import org.example.util.getDataLoader
+import org.example.util.getValueFromDataLoader
 import org.slf4j.LoggerFactory
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
@@ -18,13 +18,13 @@ data class Post(
     val content: String? = null,
     val published: Boolean = false,
 
-) {
+    ) {
     companion object {
         private val log = LoggerFactory.getLogger(Post::class.java)
     }
 
     fun author(env: DataFetchingEnvironment): CompletableFuture<User?> {
         log.info("author() called with: authorId: $authorId")
-        return env.getDataLoader(AuthorDataLoader::class).load(id)
+        return env.getValueFromDataLoader(AuthorDataLoader::class, id)
     }
 }
--- src/main/kotlin/org/example/util/BeanUtil.kt
+++ src/main/kotlin/org/example/util/BeanUtil.kt
@@ -1,11 +1,13 @@
 package org.example.util
 
 import com.expediagroup.graphql.server.execution.KotlinDataLoader
+import com.expediagroup.graphql.server.extensions.getValueFromDataLoader
 import graphql.schema.DataFetchingEnvironment
 import org.dataloader.DataLoader
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
 import org.springframework.stereotype.Component
+import java.util.concurrent.CompletableFuture
 import kotlin.reflect.KClass
 
 
@@ -50,4 +52,10 @@ class BeanUtil : ApplicationContextAware {
 inline fun <reified L : KotlinDataLoader<K, V>, K, V> DataFetchingEnvironment.getDataLoader(loaderClass: KClass<L>): DataLoader<K, V> {
     val loader = BeanUtil.getBean<L>()
     return this.getDataLoader(loader.dataLoaderName)
+}
+
+@Suppress("UNUSED_PARAMETER")
+inline fun <reified L : KotlinDataLoader<K, V>, K, V> DataFetchingEnvironment.getValueFromDataLoader(loaderClass: KClass<L>, key: K): CompletableFuture<V> {
+    val loader = BeanUtil.getBean<L>()
+    return this.getValueFromDataLoader(loader.dataLoaderName, key)
 }
